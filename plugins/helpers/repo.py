bd9acad1e7fccc2a3e5014104a67fd91ec30b06b
Nᴀʀᴜᴛᴏ <137090502+Naruto643@users.noreply.github.com>
2023-08-02 17:53:05 +0530

    Update repo.py

--- plugins/helpers/repo.py
+++ plugins/helpers/repo.py
@@ -18,9 +18,9 @@ async def git(Kashmira, message):
     r = requests.get("https://api.github.com/search/repositories", params={"q": args})
     lool = r.json()
     if lool.get("total_count") == 0:
-        await pablo.edit("File not found")
-        return
-    else:
+      await pablo.edit("File not found")
+      return
+    else:
         lol = lool.get("items")
         qw = lol[0]
         txt = f"""
