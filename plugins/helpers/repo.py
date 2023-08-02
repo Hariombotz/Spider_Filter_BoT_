b5145dad38393e257884a60d12cd90cbbc92d292
Nᴀʀᴜᴛᴏ <137090502+Naruto643@users.noreply.github.com>
2023-08-02 22:44:44 +0530

    Update repo.py

--- plugins/helpers/repo.py
+++ plugins/helpers/repo.py
@@ -10,20 +10,20 @@ from pyrogram import Client, filters
 
 @Client.on_message(filters.command('repo'))
 async def git(Kashmira, message):
-  pablo = await message.reply_text("Processing...")
-  args = message.text.split(None, 1)[1]
-  if len(message.command) == 1:
-    await pablo.edit("No input found")
-    return
-    r = requests.get("https://api.github.com/search/repositories", params={q: args})
-    lool = r.json()
-    if lool.get("total_count") == 0:
-      await pablo.edit("File not found")
-      return
+    pablo = await message.reply_text("`Processing...`")
+    args = message.text.split(None, 1)[1]
+    if len(message.command) == 1:
+        await pablo.edit("No input found")
+        return
+    r = requests.get("https://api.github.com/search/repositories", params={"q": args})
+    lool = r.json()
+    if lool.get("total_count") == 0:
+        await pablo.edit("File not found")
+        return
     else:
-        lol = lool.get("items")
-        qw = lol[0]
-        txt = f"""
+        lol = lool.get("items")
+        qw = lol[0]
+        txt = f"""
 <b>Name :</b> <i>{qw.get("name")}</i>
 
 <b>Full Name :</b> <i>{qw.get("full_name")}</i>
@@ -36,21 +36,21 @@ async def git(Kashmira, message):
 
 <b>Powed by :</b> @hbbotz
 """
-        if qw.get("description"):
-            txt += f'<b>Description :</b> <code>{qw.get("description")}</code>'
+        if qw.get("description"):
+            txt += f'<b>Description :</b> <code>{qw.get("description")}</code>'
 
-        if qw.get("language"):
-            txt += f'<b>Language :</b> <code>{qw.get("language")}</code>'
+        if qw.get("language"):
+            txt += f'<b>Language :</b> <code>{qw.get("language")}</code>'
 
-        if qw.get("size"):
-            txt += f'<b>Size :</b> <code>{qw.get("size")}</code>'
+        if qw.get("size"):
+            txt += f'<b>Size :</b> <code>{qw.get("size")}</code>'
 
-        if qw.get("score"):
-            txt += f'<b>Score :</b> <code>{qw.get("score")}</code>'
+        if qw.get("score"):
+            txt += f'<b>Score :</b> <code>{qw.get("score")}</code>'
 
-        if qw.get("created_at"):
-            txt += f'<b>Created At :</b> <code>{qw.get("created_at")}</code>'
+        if qw.get("created_at"):
+            txt += f'<b>Created At :</b> <code>{qw.get("created_at")}</code>'
 
-        if qw.get("archived") == True:
-            txt += f"<b>This Project is Archived</b>"
-        await pablo.edit(txt, disable_web_page_preview=True)
+        if qw.get("archived") == True:
+            txt += f"<b>This Project is Archived</b>"
+        await pablo.edit(txt, disable_web_page_preview=True)
