e8485007349fc558d2e681464d85c9916beac4d8
Nᴀʀᴜᴛᴏ <137090502+Naruto643@users.noreply.github.com>
2023-08-04 08:55:36 +0530

    Update password.py

--- plugins/helpers/password.py
+++ plugins/helpers/password.py
@@ -15,5 +15,5 @@ async def password(bot, update):
     limit = int(qw)
     random_value = "".join(random.sample(password, limit))
     txt = f"<b>Limit:</b> {str(limit)} \n<b>Password: <code>{random_value}</code>"
-    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Mᴋɴ Bᴏᴛᴢ™️', url='https://t.me/hbbotz')]])
+    btn = InlineKeyboardMarkup([[InlineKeyboardButton('Hʙ Bᴏᴛᴢ™️', url='https://t.me/hbbotz')]])
     await message.edit_text(text=txt, reply_markup=btn, parse_mode=enums.ParseMode.HTML)
