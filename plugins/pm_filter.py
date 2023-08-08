59d858efd9f5b7ac517552c4f2e8505d6e6e2f9d
Nᴀʀᴜᴛᴏ <137090502+Naruto643@users.noreply.github.com>
2023-08-08 14:05:14 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -62,7 +62,7 @@ async def give_filter(client, message):
                     await auto_filter(client, message) 
     
     @Client.on_message(filters.private & filters.text & filters.incoming)
-async def pv_filter(client, message):
+    async def pv_filter(client, message):
     kd = await global_filters(client, message)
     if kd == False:
         await auto_filter(client, message)
