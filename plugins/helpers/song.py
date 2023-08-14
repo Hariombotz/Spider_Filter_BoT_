34736e4f63c220494ccf57287c04182e327fa8ab
Nᴀʀᴜᴛᴏ <137090502+Naruto643@users.noreply.github.com>
2023-08-14 13:32:22 +0530

    Update song.py

--- plugins/helpers/song.py
+++ plugins/helpers/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '**𝙹𝙾𝙸𝙽 ›› [🍁 ᴄʜᴀɴɴᴇʟ 🍁](https://t.me/hbbotz)**\n**𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [muѕíc вσч](https://t.me/hbbotz)'
+        rep = '**Jᴏɪɴ ›› [ ᴄʜᴀɴɴᴇʟ ](https://t.me/hbbotz)**\n**Pᴏᴡᴇʀᴇᴅ Bʏ ›› [Hʙ Tᴇᴀᴍ](https://t.me/hbbotz)'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
