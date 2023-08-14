2b7330b842f735a1eebac2d0fbac6f0c1de66990
Nᴀʀᴜᴛᴏ <137090502+Naruto643@users.noreply.github.com>
2023-08-14 13:26:37 +0530

    Update check_alive.py

--- plugins/check_alive.py
+++ plugins/check_alive.py
@@ -61,7 +61,7 @@ async def stats(bot, update):
 
 @Client.on_message(filters.command("restart") & filters.user(ADMINS))
 async def stop_button(bot, message):
-    msg = await bot.send_message(text="**𝖡𝗈𝗍 𝖨𝗌 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝗂𝗇𝗀...🪄**", chat_id=message.chat.id)       
+    msg = await bot.send_message(text="**Bᴏᴛ Iꜱ Rᴇꜱᴛᴀʀᴛ ... 🔄**", chat_id=message.chat.id)       
     await asyncio.sleep(3)
-    await msg.edit("**𝖡𝗈𝗍 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝖾𝖽 𝖲𝗎𝖼𝖼𝖾𝗌𝗌𝖿𝗎𝗅𝗅𝗒 ! 𝖱𝖾𝖺𝖽𝗒 𝖳𝗈 𝖬𝗈𝗏𝖾 𝖮𝗇 💯**")
+    await msg.edit("**Bᴏᴛ Rᴇꜱᴛᴀʀᴛ Sᴜᴄᴄᴇꜱꜱғᴜʟʟʏ ! Rᴇᴀᴅʏ Tᴏ Mᴏᴠᴇ Oɴ 🧑‍🦯**")
     os.execl(sys.executable, sys.executable, *sys.argv)
