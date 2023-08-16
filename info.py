5ce8cb7bba3d72e758c19f9990bd4151f8c15097
Nᴀʀᴜᴛᴏ <137090502+Naruto643@users.noreply.github.com>
2023-08-16 10:00:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,14 +50,11 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
 VERIFY = bool(environ.get('IS_VERIFY', False))
-IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'True')), True)
-VERIFY2_URL = environ.get('VERIFY2_URL', "tinyfy.in")
-VERIFY2_API = environ.get('VERIFY2_API', "3788f44ed9c2846be08ee987fcd6a1db711f252d")
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tinyfy.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '3788f44ed9c2846be08ee987fcd6a1db711f252d')
 SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'tinyfy.in')
 SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '3788f44ed9c2846be08ee987fcd6a1db711f252d')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
+IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
 MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
