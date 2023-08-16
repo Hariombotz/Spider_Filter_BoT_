f612eca07719dc5ecba56c83143b8d80277aef01
Nᴀʀᴜᴛᴏ <137090502+Naruto643@users.noreply.github.com>
2023-08-16 09:30:43 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,11 +49,13 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Hbbotz")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-VERIFY = bool(environ.get('IS_VERIFY', True))
-SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'tinyfy.in')
-SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '3788f44ed9c2846be08ee987fcd6a1db711f252d')
+IS_VERIFY = bool(environ.get('IS_VERIFY', True))
+VERIFY2_URL = environ.get('VERIFY2_URL', "tinyfy.in")
+VERIFY2_API = environ.get('VERIFY2_API', "3788f44ed9c2846be08ee987fcd6a1db711f252d")
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tinyfy.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', '3788f44ed9c2846be08ee987fcd6a1db711f252d')
+SECOND_SHORTLINK_URL = environ.get('SECOND_SHORTLINK_URL', 'tinyfy.in')
+SECOND_SHORTLINK_API = environ.get('SECOND_SHORTLINK_API', '3788f44ed9c2846be08ee987fcd6a1db711f252d')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
