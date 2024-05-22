840bdc6b82d4d518b259e707c4dea69bc72d4815
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2024-05-22 12:55:56 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -58,10 +58,6 @@ async def start(client, message):
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
         )
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
