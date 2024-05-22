063d8d44cff1f1de857a8f49e31c022da9a09b7a
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2024-05-22 13:00:53 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -58,9 +58,14 @@ async def start(client, message):
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_photo(
+            photo=random.choice(PICS),
+            caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
+            reply_markup=reply_markup,
+            parse_mode=enums.ParseMode.HTML  
+       
         )
         return
-    if AUTH_CHANNEL and not await is_subscribed(client, message):
+   if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
             invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except ChatAdminRequired:
