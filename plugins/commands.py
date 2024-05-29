0a717474a4b3fcd50603f3f6ca20b60adc1ed5a0
mksir12 <116490353+mksir12@users.noreply.github.com>
2024-05-29 10:36:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -598,7 +598,7 @@ async def channel_info(bot, message):
 async def log_file(bot, message):
     """Send log file"""
     try:
-        await message.reply_document('Telegram Bot Logs.txt')
+        await message.reply_document('TelegramBot.log')
     except Exception as e:
         await message.reply(str(e))
 
