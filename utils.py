d0fa74fbee699155a6deb5125c7a0d8f2938c546
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2024-05-31 16:13:35 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -55,19 +55,35 @@ class temp(object):
     IMDB_CAP = {}
 
 async def is_subscribed(bot, query):
-    if await db.find_join_req(query.from_user.id):
-        return True
-    try:
-        user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
-    except UserNotParticipant:
-        pass
-    except Exception as e:
-        logger.exception(e)
+    if REQUEST_TO_JOIN_MODE == True and join_db().isActive():
+        try:
+            user = await join_db().get_user(query.from_user.id)
+            if user and user["user_id"] == query.from_user.id:
+                return True
+            else:
+                try:
+                    user_data = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+                except UserNotParticipant:
+                    pass
+                except Exception as e:
+                    logger.exception(e)
+                else:
+                    if user_data.status != enums.ChatMemberStatus.BANNED:
+                        return True
+        except Exception as e:
+            logger.exception(e)
+            return False
     else:
-        if user.status != enums.ChatMemberStatus.BANNED:
-            return True
-
-    return False
+        try:
+            user = await bot.get_chat_member(AUTH_CHANNEL, query.from_user.id)
+        except UserNotParticipant:
+            pass
+        except Exception as e:
+            logger.exception(e)
+        else:
+            if user.status != enums.ChatMemberStatus.BANNED:
+                return True
+        return False
 
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
