532bec910595bc066c97f6ca98df4c451ee59894
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2024-06-07 14:00:44 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -470,7 +470,7 @@ async def start(client, message):
     if not files_:
         pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
-            if not await check_verification(client, message.from_user.id) and await db.has_premium_access(message.from_user.id): and VERIFY == True:
+            if not await check_verification(client, message.from_user.id) and await db.has_premium_access(message.from_user.id):
                 btn = [[
                     InlineKeyboardButton("♻️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴠᴇʀɪꜰʏ ♻️", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
                 ],[
