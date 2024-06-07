ec426726ee1f8fce3f8aa900f36d247195282551
Aman vishwakarma <151424799+Botsthe@users.noreply.github.com>
2024-06-07 11:59:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -397,7 +397,7 @@ async def start(client, message):
             if f_caption is None:
                 f_caption = f"{' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files1.file_name.split()))}"
 
-            if not await check_verification(client, message.from_user.id) and not await db.has_premium_access(user_id): and VERIFY == True:
+            if not await check_verification(client, message.from_user.id) and await db.has_premium_access(user_id): and VERIFY == True:
                 btn = [[
                     InlineKeyboardButton("♻️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴠᴇʀɪꜰʏ ♻️", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
                 ],[
@@ -470,7 +470,7 @@ async def start(client, message):
     if not files_:
         pre, file_id = ((base64.urlsafe_b64decode(data + "=" * (-len(data) % 4))).decode("ascii")).split("_", 1)
         try:
-            if not await check_verification(client, message.from_user.id) and not await db.has_premium_access(user_id): and VERIFY == True:
+            if not await check_verification(client, message.from_user.id) and await db.has_premium_access(user_id): and VERIFY == True:
                 btn = [[
                     InlineKeyboardButton("♻️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴠᴇʀɪꜰʏ ♻️", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
                 ],[
@@ -529,7 +529,7 @@ async def start(client, message):
     if f_caption is None:
         f_caption = f" {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
 
-    if not await check_verification(client, message.from_user.id) and not await db.has_premium_access(user_id): and VERIFY == True:
+    if not await check_verification(client, message.from_user.id) and await db.has_premium_access(user_id): and VERIFY == True:
         btn = [[
             InlineKeyboardButton("♻️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴠᴇʀɪꜰʏ ♻️", url=await get_token(client, message.from_user.id, f"https://telegram.me/{temp.U_NAME}?start="))
         ],[
