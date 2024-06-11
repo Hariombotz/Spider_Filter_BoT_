c4786ba4dc1738111fd7365fcbb73f114da9765f
Aman vishwakarma <151424799+Botsthe@users.noreply.github.com>
2024-06-11 09:19:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1447,7 +1447,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text="𝑫𝒐𝒏𝒆 ✅️"
         )
         await query.message.edit_text(
-            text=script.START_TXT.format(query.from_user.mention, temp.U_NAME, temp.B_NAME),
+            text=script.START_TXT.format(query.from_user.mention, query.from_user.id, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
