986c5217b8c187486af64c59c081b466d7c5af77
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2024-06-16 20:42:04 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -47,8 +47,8 @@ SPELL_CHECK = {}
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
-    await message.react(emoji=random.choice(REACTIONS))
-        if message.chat.id != SUPPORT_CHAT_ID:
+await message.react(emoji=random.choice(REACTIONS))
+if message.chat.id != SUPPORT_CHAT_ID:
         manual = await manual_filters(client, message)
         if manual == False:
             settings = await get_settings(message.chat.id)
