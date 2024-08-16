2bbb57a5cd7d24e27bd3d96d80adc23375e84b3e
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2024-08-16 23:20:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -46,7 +46,7 @@ BUTTONS2 = {}
 SPELL_CHECK = {}
 # ENABLE_SHORTLINK = ""
 
-@Client.on_message((filters.group | filters.private) & filters.text & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     user = message.from_user.first_name
     user_id = message.from_user.id
