39cde2a13c9132bd3cfecf3bec82236cdf6748d3
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2024-08-18 10:46:10 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -381,7 +381,6 @@ async def start(client, message):
                     f_caption=f_caption
             if f_caption is None:
                 f_caption = f"{' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files1.file_name.split()))}"
-            if not await db.has_premium_access(message.from_user.id):
             
             if not await check_verification(client, message.from_user.id)  and VERIFY == True: 
                 btn = [[
