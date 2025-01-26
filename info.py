d36200a49c79efc071aabb03556b5be247241c86
Hariom Singh <168641603+Hariombotz@users.noreply.github.com>
2025-01-26 21:42:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -114,4 +114,4 @@ LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 #start_command_reactions
-REACTIONS = ["🤝", "🤗", "😍", "😈", "⚡️", "🔥", "👀", "🌚", "🆒", "👻"] #don't add any emoji because tg not support all emoji reactions
+REACTIONS = ["🩸", "🫧", "❤️‍🔥", "🫀"] #don't add any emoji because tg not support all emoji reactions
